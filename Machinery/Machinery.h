f43dc9b1be336a47f8beda64694b73b63460971c
salimbraksa <salim.braksa@gmail.com>
2017-02-19 03:05:38 +0000

    Initial Commit

---
+++ Machinery/Machinery.h
@@ -0,0 +1,19 @@
+//
+//  Machinery.h
+//  Machinery
+//
+//  Created by Salim Braksa on 2/19/17.
+//  Copyright © 2017 Salim Braksa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Machinery.
+FOUNDATION_EXPORT double MachineryVersionNumber;
+
+//! Project version string for Machinery.
+FOUNDATION_EXPORT const unsigned char MachineryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Machinery/PublicHeader.h>
+
+
